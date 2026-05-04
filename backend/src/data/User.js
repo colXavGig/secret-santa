be2330e68cf8198d6ef88b4dc54208a423b650b5
Xavier Giguere <bebewold@gmail.com>
2026-05-04 00:18:07 -0400

    fix: await bcrypt hash operation in user model to prevent promise return instead of hashed string

--- backend/src/data/User.js
+++ backend/src/data/User.js
@@ -11,7 +11,7 @@ UserSchema.pre('save', async function() {
     if (!this.isModified('password')) return;
     try {
         const salt = await bcrypt.genSalt(10);
-        this.password = bcrypt.hash(this.password, salt);
+        this.password = await bcrypt.hash(this.password, salt);
     } catch (err) {
         throw err;
     }
